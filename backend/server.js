b505b9558a104ce23db5de89f1927303e3899b03
rishu <rrishu212@gmail.com>
2024-08-31 23:46:47 -0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import { app, server } from "./socket/socket.js"
 dotenv.config();
 
 // const app = express();
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 
@@ -23,7 +23,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes )
+app.use("/api/users", userRoutes );
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
