5dfa5894bfc3db689ebe35b7e263324720fc0c36
rishu <rrishu212@gmail.com>
2024-08-31 23:31:10 -0700

    4th-part completed socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,20 +1,12 @@
 import Conversation from "../models/conversation.model.js";
-import Message from "../models/message.model.js"; 
+import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
 	try {
-		const { message} = req.body;
+		const { message } = req.body;
 		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
-
-        console.log(req.body);
-        console.log(message);
-
-        if (!message) {
-            return res.status(400).json({ error: "Message is required" });
-          }
-
-          console.log(message);
 
 		let conversation = await Conversation.findOne({
 			participants: { $all: [senderId, receiverId] },
@@ -43,11 +35,11 @@ export const sendMessage = async (req, res) => {
 		await Promise.all([conversation.save(), newMessage.save()]);
 
 		// SOCKET IO FUNCTIONALITY WILL GO HERE
-		// const receiverSocketId = getReceiverSocketId(receiverId);
-		// if (receiverSocketId) {
-		// 	// io.to(<socket_id>).emit() used to send events to specific client
-		// 	io.to(receiverSocketId).emit("newMessage", newMessage);
-		// }
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js"
 
 dotenv.config();
 
-const app = express();
+// const app = express();
+//const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 
@@ -23,7 +26,7 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes )
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server Running on port ${PORT}`)
 });
