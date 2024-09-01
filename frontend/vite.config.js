e14da47c713c834f4440010f38a1b1be6f74dcda
rishu <rrishu212@gmail.com>
2024-09-01 00:20:03 -0700

    latest update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
      port: 3000,
      proxy: {
 			"/api": {
-				target: "https://chattt-app-7v9i.onrender.com/",
+				target: "http://localhost:5000",
 			},
 		},
   },
